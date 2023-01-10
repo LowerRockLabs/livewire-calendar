78a05fa3953c12978c44b3afb2f5a4298735add4
Joe <joe@lowerrocklabs.com>
2023-01-10 22:38:15 +0000

    Adding Start Time

--- resources/views/event.blade.php
+++ resources/views/event.blade.php
@@ -1,13 +1,17 @@
-<div
-    @if($eventClickEnabled)
-        wire:click.stop="onEventClick('{{ $event['id']  }}')"
-    @endif
-    class="bg-white rounded-lg border py-2 px-3 shadow-md cursor-pointer">
-
-    <p class="text-sm font-medium">
-        {{ $event['title'] }}
-    </p>
-    <p class="mt-2 text-xs">
+<div @if ($eventClickEnabled) wire:click.stop="onEventClick('{{ $event['id'] }}')" @endif
+    class="flex flex-wrap px-3 py-2 text-black bg-white border rounded-lg shadow-md cursor-pointer">
+    <div class="flex w-full">
+        @if ($this->showStartTimeEnabled)
+            <div class="inline w-auto place-self-center ">
+                {{ \Carbon\Carbon::parse($event['date'])->format('H:i') }}
+            </div>
+        @endif
+        <div class="flex-auto inline pl-2 text-sm font-medium text-left place-self-center">
+            {{ $event['title'] }}
+        </div>
+    </div>
+    <div class="w-full mt-2 text-xs">
         {{ $event['description'] ?? 'No description' }}
-    </p>
+
+    </div>
 </div>
